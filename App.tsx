d89d9b88107dae7669ce93cc42faaf6bcd1d4358
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-07-13 18:36:15 +0700

    Add some colors

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react"
-import { StyleSheet, Text, View, Button } from "react-native"
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import HomePage from './ui/home/HomePage'
 
 export default function App() {
   const [count, setCount] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Hello Jarmotion {count}</Text>
-      <Button title="Add Count" onPress={() => setCount(count + 1)} />
+      {/* <Text>Hello Jarmotion {count}</Text>
+      <Button title="Add Count" onPress={() => setCount(count + 1)} /> */}
+      <HomePage />
     </View>
   )
 }
@@ -14,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
