f3bdbc783905e12796f7ddfba67bde48561598ee
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-08-04 13:23:52 +0700

    Add auth services

---
+++ ui/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+
+const LoginPage = () => {
+  return (
+    <View>
+      <Text>This is login page</Text>
+    </View>
+  )
+}
+
+export default LoginPage
