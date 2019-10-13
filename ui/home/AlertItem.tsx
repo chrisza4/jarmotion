d2e8020a6d6abc1d723a1eb3dfc681ec9e870faa
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-10-13 18:55:59 +0700

    Refactor alertItem out to file

---
+++ ui/home/AlertItem.tsx
@@ -0,0 +1,60 @@
+import moment from 'moment'
+import React from 'react'
+import styled from 'styled-components/native'
+import { isUnacknowledge } from '../../domains/alert/AlertFunc'
+import { IDisplayAlertItem } from '../../domains/alert/AlertTypes'
+
+interface IAlertItemProps {
+  displayAlert: IDisplayAlertItem
+}
+
+interface IAlertStatus {
+  unack: boolean
+}
+
+const AlertItemWrapper = styled.View<IAlertStatus>`
+  border-radius: 10px;
+  border-color: ${props => (props.unack ? 'red' : 'silver')}
+  border-width: ${props => (props.unack ? 1 : 0.3)};
+  border-style: solid;
+  height: 80px;
+  margin-top: 10px;
+  padding: 9px;
+`
+
+const AlertItemTimeWrapper = styled.View`
+  flex-direction: row;
+`
+
+const AlertItemTimeText = styled.Text<IAlertStatus>`
+  font-family: poppins-light;
+  ${props => (props.unack ? 'color: red;' : '')}
+`
+
+const AlertMessageTextWrapper = styled.View`
+  margin-top: 10px;
+`
+const AlertMessageText = styled.Text<IAlertStatus>`
+  font-family: poppins-bold;
+  ${props => (props.unack ? 'color: red;' : '')}
+`
+
+const AlertItem = (props: IAlertItemProps) => {
+  const unack = isUnacknowledge(props.displayAlert)
+  return (
+    <AlertItemWrapper unack={unack}>
+      <AlertItemTimeWrapper>
+        <AlertItemTimeText unack={unack}>
+          {moment(props.displayAlert.sentAt).fromNow()}
+        </AlertItemTimeText>
+      </AlertItemTimeWrapper>
+      <AlertMessageTextWrapper>
+        <AlertMessageText unack={unack}>
+          {props.displayAlert.message}
+        </AlertMessageText>
+      </AlertMessageTextWrapper>
+    </AlertItemWrapper>
+  )
+}
+
+export default AlertItem
--- ui/home/AlertModal.tsx
+++ ui/home/AlertModal.tsx
@@ -1,12 +1,11 @@
 import _ from 'lodash'
-import moment from 'moment'
 import React from 'react'
 import { View } from 'react-native'
 import styled from 'styled-components/native'
-import { isUnacknowledge } from '../../domains/alert/AlertFunc'
 import { IDisplayAlertItem } from '../../domains/alert/AlertTypes'
 import TextButton, { TextButtonStyle } from '../uikit/buttons/TextButton'
 import Modal from '../uikit/Modal'
+import AlertItem from './AlertItem'
 
 interface IAlertModalProps {
   alerts: IDisplayAlertItem[]
@@ -58,59 +57,6 @@ const AlertModal = (props: IAlertModalProps) => {
         </ModalBody>
       </ModalContent>
     </Modal>
-  )
-}
-
-interface IAlertItemProps {
-  displayAlert: IDisplayAlertItem
-}
-
-interface IAlertStatus {
-  unack: boolean
-}
-
-const AlertItemWrapper = styled.View<IAlertStatus>`
-  border-radius: 10px;
-  border-color: ${props => (props.unack ? 'red' : 'silver')}
-  border-width: ${props => (props.unack ? 1 : 0.3)};
-  border-style: solid;
-  height: 80px;
-  margin-top: 10px;
-  padding: 9px;
-`
-
-const AlertItemTimeWrapper = styled.View`
-  flex-direction: row;
-`
-
-const AlertItemTimeText = styled.Text<IAlertStatus>`
-  font-family: poppins-light;
-  ${props => (props.unack ? 'color: red;' : '')}
-`
-
-const AlertMessageTextWrapper = styled.View`
-  margin-top: 10px;
-`
-const AlertMessageText = styled.Text<IAlertStatus>`
-  font-family: poppins-bold;
-  ${props => (props.unack ? 'color: red;' : '')}
-`
-
-const AlertItem = (props: IAlertItemProps) => {
-  const unack = isUnacknowledge(props.displayAlert)
-  return (
-    <AlertItemWrapper unack={unack}>
-      <AlertItemTimeWrapper>
-        <AlertItemTimeText unack={unack}>
-          {moment(props.displayAlert.sentAt).fromNow()}
-        </AlertItemTimeText>
-      </AlertItemTimeWrapper>
-      <AlertMessageTextWrapper>
-        <AlertMessageText unack={unack}>
-          {props.displayAlert.message}
-        </AlertMessageText>
-      </AlertMessageTextWrapper>
-    </AlertItemWrapper>
   )
 }
 
