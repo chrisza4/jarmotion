4dce76fc8426648f2c11ae4bfe9d1bf422bb9fe1
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-10-18 18:02:19 +0700

    Add Sensing Page layout

---
+++ ui/layouts/PageLayout.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { ImageBackground, StyleSheet, View } from 'react-native'
+import PageTitle from '../layouts/PageTitle'
+import ScreenLayout from '../layouts/ScreenLayout'
+import { offWhite } from '../styles/colors'
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: offWhite,
+    height: '100%',
+    justifyContent: 'space-between'
+  },
+  backgroundImage: {
+    height: 188,
+    width: '100%',
+    resizeMode: 'stretch',
+    backgroundColor: 'transparent'
+  }
+})
+
+interface IPageLayoutProps {
+  titleElement: React.ReactNode
+  children: React.ReactNode
+}
+
+const PageLayout = (props: IPageLayoutProps) => {
+  const renderTopSection = () => (
+    <ImageBackground
+      style={styles.backgroundImage}
+      source={require('../../assets/curvy_top_bg.png')}
+    >
+      <PageTitle>{props.titleElement}</PageTitle>
+      <View style={{}}>{props.children}</View>
+    </ImageBackground>
+  )
+
+  return (
+    <ScreenLayout>
+      <View style={styles.page}>{renderTopSection()}</View>
+    </ScreenLayout>
+  )
+}
+
+export default PageLayout
--- ui/sensing/SensingPage.tsx
+++ ui/sensing/SensingPage.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
-import { Text } from 'react-native'
-import PageTitle from '../layouts/PageTitle'
-import ScreenLayout from '../layouts/ScreenLayout'
+import { View } from 'react-native'
+import styled from 'styled-components/native'
+import PageLayout from '../layouts/PageLayout'
+import { brownishGrey, fontBlack } from '../styles/colors'
+
+const PageTitleText = styled.Text`
+  color: ${fontBlack};
+  font-family: poppins-bold;
+  font-size: 21px;
+`
+
+const PageDescription = styled.Text`
+  align-self: center;
+  margin-top: 20px;
+  color: ${brownishGrey};
+  font-family: poppins-medium;
+  font-size: 15px;
+`
 
 const SensingPage = () => {
   return (
-    <ScreenLayout>
-      <PageTitle>
-        <Text>Sensing</Text>
-      </PageTitle>
-    </ScreenLayout>
+    <PageLayout titleElement={<PageTitleText>Sensing</PageTitleText>}>
+      <View>
+        <PageDescription>
+          Receive an alert when your parter feel these!
+        </PageDescription>
+      </View>
+    </PageLayout>
   )
 }
 
