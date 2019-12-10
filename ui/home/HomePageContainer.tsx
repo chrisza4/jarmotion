792b7be926082605a3634e55bc121c42f22e5006
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-12-10 18:38:19 +0700

    Navigate to lover when no lover

--- ui/home/HomePageContainer.tsx
+++ ui/home/HomePageContainer.tsx
@@ -1,26 +1,46 @@
 import { observer } from 'mobx-react'
 import React, { useEffect } from 'react'
+import { View } from 'react-native'
+import { NavigationEvents } from 'react-navigation'
 import EmojiStore from '../../stores/EmojiStore'
 import UserStore from '../../stores/UserStore'
+import { INavigationComponentProps } from '../../types/NavigationTypes'
 import HomePage from './HomePage'
 
 const HomePageContainer = observer(
-  (): JSX.Element => {
+  (props: INavigationComponentProps): JSX.Element => {
     useEffect(() => {
+      refresh()
+    }, [])
+
+    const navigateToLoverIfNeeded = () => {
+      if (!UserStore.couple?.id) {
+        props.navigation.navigate({ routeName: 'Lover' })
+      }
+    }
+    const refresh = () => {
       UserStore.init().then(() => {
         EmojiStore.loadEmoji(UserStore.me.id)
         EmojiStore.loadEmoji(UserStore.couple.id)
       })
-    }, [])
+    }
 
     return (
-      <HomePage
-        emojis={EmojiStore.emojis}
-        addEmojis={EmojiStore.addEmojis}
-        loadState={EmojiStore.getLoadStateByUserId(UserStore.couple.id)}
-        users={UserStore.users}
-        loverUserId={UserStore.couple.id}
-      />
+      <View>
+        <HomePage
+          emojis={EmojiStore.emojis}
+          addEmojis={EmojiStore.addEmojis}
+          loadState={EmojiStore.getLoadStateByUserId(UserStore.couple.id)}
+          users={UserStore.users}
+          loverUserId={UserStore.couple.id}
+        />
+        <NavigationEvents
+          onDidFocus={() => {
+            navigateToLoverIfNeeded()
+            refresh()
+          }}
+        />
+      </View>
     )
   }
 )
